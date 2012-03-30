2b3866735ea00d3618b3c8f63bb0d9e3378a9b8a
Simon Lipp <sloonz@gmail.com>
2012-03-30 16:29:49 +0200

    Initial commit

---
+++ maildir-feed-rss/maildir-feed-rss.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/sloonz/cfeedparser"
+	"github.com/sloonz/go-maildir"
+	"github.com/sloonz/go-mime-message"
+	"github.com/sloonz/go-qprintable"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type Cache struct {
+	data map[string]bool
+	path string
+}
+
+func (c *Cache) load() error {
+	cacheFile, err := os.Open(c.path)
+	if err != nil {
+		return err
+	}
+
+	data, err := ioutil.ReadAll(cacheFile)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, &c.data)
+}
+
+func (c *Cache) dump() error {
+	cacheFile, err := os.Create(c.path + ".new")
+	if err != nil {
+		return err
+	}
+	defer cacheFile.Close()
+
+	enc := json.NewEncoder(cacheFile)
+	if err = enc.Encode(c.data); err != nil {
+		return err
+	}
+
+	return os.Rename(c.path+".new", c.path)
+}
+
+var cache Cache
+
+func firstNonEmpty(s ...string) string {
+	var val string
+	for _, val = range s {
+		if val != "" {
+			break
+		}
+	}
+	return val
+}
+
+func getRFC822Date(e *feedparser.Entry) string {
+	emptyTime := time.Time{}
+	if e.PublicationDateParsed != emptyTime {
+		return e.PublicationDateParsed.Format(time.RFC822)
+	}
+	if e.ModificationDateParsed != emptyTime {
+		return e.ModificationDateParsed.Format(time.RFC822)
+	}
+	if e.PublicationDate != "" {
+		return e.PublicationDate
+	}
+	if e.ModificationDate != "" {
+		return e.ModificationDate
+	}
+	return time.Now().UTC().Format(time.RFC822)
+}
+
+func getFrom(e *feedparser.Entry) string {
+	name := strings.TrimSpace(message.EncodeWord(firstNonEmpty(e.Author.Name, e.Author.Uri, e.Author.Text)))
+	if e.Author.Email != "" {
+		name += " <" + strings.TrimSpace(e.Author.Email) + ">"
+	}
+	return name
+}
+
+var convertEOLReg = regexp.MustCompile("\r\n?")
+
+func convertEOL(s string) string {
+	return convertEOLReg.ReplaceAllString(s, "\n")
+}
+
+func process(rawUrl string) error {
+	url_, err := url.Parse(rawUrl)
+	if err != nil {
+		return err
+	}
+
+	md, err := maildir.New(".", false)
+	if err != nil {
+		return err
+	}
+
+	feed, err := feedparser.ParseURL(url_)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("[%s]\n", feed.Title)
+	for _, entry := range feed.Entries {
+		postId := firstNonEmpty(entry.Id, entry.Link, entry.PublicationDate+":"+entry.Title)
+		if _, hasId := cache.data[postId]; hasId {
+			continue
+		}
+
+		body := convertEOL(firstNonEmpty(entry.Content, entry.Summary))
+		body += "\n<p><small><a href=\"" + entry.Link + "\">View post</a></small></p>\n"
+
+		title := strings.TrimSpace(entry.Title)
+		msg := message.NewTextMessage(qprintable.UnixTextEncoding, bytes.NewBufferString(body))
+		msg.SetHeader("Date", getRFC822Date(&entry))
+		msg.SetHeader("From", getFrom(&entry))
+		msg.SetHeader("To", "Feeds <feeds@localhost>")
+		msg.SetHeader("Subject", message.EncodeWord(title))
+		msg.SetHeader("Content-Type", "text/html")
+
+		_, err = md.CreateMail(msg)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("  %s\n", title)
+		cache.data[postId] = true
+	}
+
+	return nil
+}
+
+func main() {
+	url_ := os.Args[1]
+
+	cache.path = path.Join(os.Getenv("HOME"), ".cache", "rss2maildir", strings.Replace(url_, "/", "_", -1))
+	cache.data = make(map[string]bool)
+
+	err := cache.load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Warning: can't read cache: %s\n", err.Error())
+	}
+
+	err = process(url_)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't process feed: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	err = cache.dump()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't write cache: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
---
+++ maildir-feed/maildir-feed.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/sloonz/go-maildir"
+	"github.com/sloonz/go-mime-message"
+	"github.com/sloonz/go-qprintable"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path"
+	"syscall"
+	"time"
+)
+
+const RUN_DELAY = 2
+const INTERVAL_DELAY = 60 * 15
+const MAX_ERR = 10
+
+func Abs(p string) string {
+	if path.IsAbs(p) {
+		return p
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err.Error())
+	}
+	return path.Join(wd, p)
+}
+
+func worker(root, md *maildir.Maildir, url_ *url.URL, delay int) {
+	errCount := 0
+	dir, _ := path.Split(os.Args[0])
+	execPath := path.Join(Abs(dir), "maildir-feed-rss")
+
+	time.Sleep(time.Duration(int64(delay) * 1e9))
+	for {
+		cmd := exec.Command(execPath, url_.String())
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = nil
+		err := cmd.Run()
+
+		if err != nil {
+			errCount++
+			fmt.Fprintf(os.Stderr, "[%s: error: %s]\n", url_.String(), err.Error())
+			if errCount >= MAX_ERR {
+				errCount = 0
+
+				msgText := fmt.Sprintf("Too many errors while fetching %s\n", url_.String())
+				msg := message.NewTextMessage(qprintable.UnixTextEncoding, bytes.NewBufferString(msgText))
+				msg.SetHeader("Date", time.Now().UTC().Format(time.RFC822))
+				msg.SetHeader("From", "Feeds <feeds@localhost>")
+				msg.SetHeader("To", "Feeds <feeds@localhost>")
+				msg.SetHeader("Subject", "Error while fetching "+url_.String())
+				msg.SetHeader("Content-Type", "text/plain; encoding=UTF-8")
+
+				_, err = root.CreateMail(msg)
+				if err != nil {
+					panic(err.Error())
+				}
+			}
+		}
+
+		time.Sleep(time.Duration(INTERVAL_DELAY * 1e9))
+	}
+}
+
+func parseBox(root, md *maildir.Maildir, data map[string]interface{}, delay int) int {
+	for name, v := range data {
+		child, err := md.Child(name, true)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Can't open %s: %s\n", name, err.Error())
+			os.Exit(1)
+		}
+
+		switch v.(type) {
+		case map[string]interface{}:
+			delay = parseBox(root, child, v.(map[string]interface{}), delay)
+		case []interface{}:
+			for i, item := range v.([]interface{}) {
+				url_, ok := item.(string)
+				if !ok {
+					fmt.Fprintf(os.Stderr, "%s[%i]: bad value type", name, i)
+					os.Exit(1)
+				}
+				parsedURL, err := url.Parse(url_)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s[%i]: bad url: %s", name, i, err)
+					os.Exit(1)
+				}
+				go worker(root, child, parsedURL, delay)
+				delay += RUN_DELAY
+			}
+		case string:
+			parsedURL, err := url.Parse(v.(string))
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: bad url: %s", name, err)
+				os.Exit(1)
+			}
+			go worker(root, child, parsedURL, delay)
+			delay += RUN_DELAY
+		default:
+			fmt.Fprintf(os.Stderr, "%s: bad value type", name)
+			os.Exit(1)
+		}
+	}
+	return delay
+}
+
+func main() {
+	// Read & parse config
+	configFile, err := os.Open(path.Join(os.Getenv("HOME"), ".config", "rss2maildir", "feeds.json"))
+	config := make(map[string]interface{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't open config\n")
+		return
+	}
+	data, err := ioutil.ReadAll(configFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't read config\n")
+		return
+	}
+	if err = json.Unmarshal(data, &config); err != nil {
+		fmt.Fprintf(os.Stderr, "Parse error: %s\n", err.Error())
+		return
+	}
+
+	md, err := maildir.New(path.Join(os.Getenv("HOME"), "Maildir-feeds"), true)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't open maildir: %s\n", err.Error())
+		return
+	}
+
+	parseBox(md, md, config, 0)
+
+	// Wait for SIGINT
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGINT)
+	for {
+		<-sigChan
+		break
+	}
+}
